0281d7a1865e2ef3201222579a3b93fc150c8824
Andrey Maslennikov <v-andrem@microsoft.com>
2019-06-28 12:44:11 +0300

    Fix dependency name

--- IPInfoExample/Package.swift
+++ IPInfoExample/Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "IPInfoExample",
-            dependencies: ["Alamofire", "SwiftyJSON", "vapor"]),
+            dependencies: ["Alamofire", "SwiftyJSON", "Vapor"]),
         .testTarget(
             name: "IPInfoExampleTests",
             dependencies: ["IPInfoExample"]),
